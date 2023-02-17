71d554bb78cf1ac480699dab8d353b97c167d8d3
Bob Hyman <bob.hyman@gmail.com>
2023-02-16 23:12:44 -0500

    Simplify files, document entrypoints, add example

---
+++ examples/padding.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+use print_positions::print_positions;
+
+/// Demonstrating the convenience of grapheme cluster length arithmetic
+/// when used for padding or filling fixed width fields
+/// for display on a screen with monospace fonts and unicode + emoji support.
+///
+fn pad_field<'a>(input: &'a str, width: usize, fill: &str) {
+    let padding = fill.repeat(width);
+    let segs: Vec<_> = print_positions(input).collect();
+
+    assert_eq!(
+        input,
+        segs.join(""),
+        "print position segmentation doesn't lose / insert characters"
+    );
+
+    println!(
+        "Content of this field is {} chars long but {} print positions wide",
+        input.len(),
+        segs.len(),
+    );
+    println!("   padded to width {width} with `{fill}`");
+    println!("    {}{}", &padding[..(width - segs.len())], input);
+    println!("    {}", padding);
+}
+
+fn main() -> Result<()> {
+    println!("Padded field containing family emoji (3-code-point grapheme cluster, with zero width joiner)");
+    // "family" emoji: woman ZWJ laptop == hacker? a single print position.
+    pad_field("abc\u{1f469}\u{200d}\u{1f4bb}", 5, "+");
+    println!(
+        "Note:  emoji display is probably broken in your terminal window, few support ZWJ correctly."
+    );
+    println!("Try it in rust playground, or other web screen.\n");
+
+    println!("Same padding works when coloring added.");
+    let colorful = &vec![
+        "a\u{1b}[30;42m",
+        "b\u{1b}[30;45m",
+        "c\u{1b}[0m",
+        "\u{1f469}\u{200d}\u{1f4bb}",
+    ]
+    .join("");
+    pad_field(colorful, 5, "+"); // extra rendering doesn't change padding
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,402 @@
-use simple_logging;
-use log::LevelFilter;
+//! A print position is a slice of string which a user would read as a single 'character',
+//! occupying one column on the screen or printed page.  As implemented by this iterator,
+//! the slice contains a *single* extended grapheme cluster as defined by
+//! [UAX#29](http://www.unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries)
+//! surrounded by any
+//! [ANSI escape codes](https://en.wikipedia.org/wiki/ANSI_escape_code#Description)
+//! found surrounding the grapheme in the string.  
+//!
+//! This is handy for laying out text on an Ansi-compatible console screen,
+//! where ANSI SGR codes are used to set text colors and intensity and the text
+//! is non-ASCII unicode.  You can count the returned graphemes as one column
+//! (print position) each and pad the rest of the field with blanks to the desired visual width.
+//!  
+//! Most ANSI sequences change the rendering of the characters that *follow* it, so they are
+//! embedded *at the beginning* of the returned grapheme.  
+//! However, codes that *reset* special rendering modes, such as SGR reset and RIS,
+//! are embedded *at the end*.  
+//! This allows caller to insert an undecorated string into the middle of a
+//! decorated string (e.g, SGR _something_ _text_ SGR RESET)  without having to track the rendering state, so long as the
+//! insertion is done on print position boundaries.
+//!
+//! Note that some ANSI control codes and escape sequences do move the cursor on a compatible device.
+//! This package does not attempt to track the cursor position, it simply
+//! passes all these characters through to the output grapheme unmodified.
+//! This way of handling embedded control codes such as backspace and tab is arguably a bug.
 
-pub use unicode_segmentation::UnicodeSegmentation;
+use unicode_segmentation::{GraphemeIndices, UnicodeSegmentation};
 
-fn main() {
+/// Iterator which retuns "print positions" found in a string.
+/// ```rust
+/// use print_positions::print_positions;
+///
+/// // Segmentation via extended grapheme clusters
+/// let segs: Vec<_> = print_positions("abc\u{1f468}\u{200d}\u{1f467}\u{200d}\u{1f466}").collect();
+/// assert_eq!(vec!("a","b","c", "\u{1f468}\u{200d}\u{1f467}\u{200d}\u{1f466}"), segs);
+///
+/// // Control chars and ANSI escapes *included within* grapheme clusters
+/// let segs: Vec<_> = print_positions("abc\u{1b}[37;46mdef\u{1b}[0mg").collect();
+/// assert_eq!(vec!("a","b","c", "\u{1b}[37;46md","e","f\u{1b}[0m", "g"), segs);
+///
+/// // Align input in fixed-size field (assuming monospace unicode font, even for emoji)
+/// // Also -- combined emoji does not render correctly in linux terminal, try rust playground instead.
+/// let sample = "abc\u{1f469}\u{200d}\u{1f4bb}"; // woman ZWJ laptop == hacker? a single print position.
+/// let segs: Vec<_> = print_positions(sample).collect();
+/// assert_eq!( segs.len(), 4, "expecting 4 grapheme clusters in result");
+///
+/// let padding = "----------";
+/// let cur_pad = &padding[..padding.len() - segs.len()];
+/// let segs_str = &segs.join("");
+/// println!("This field is 10 chars wide: {}{}", cur_pad, &segs.join(""));
+/// println!("    fixed width underlining  {}", padding);
+///
+pub struct PrintPositions<'a>(PrintPositionIndices<'a>);
 
-    simple_logging::log_to_stderr(LevelFilter::Info);
+impl<'a> PrintPositions<'a> {}
 
-}
+impl<'a> Iterator for PrintPositions<'a> {
+    type Item = &'a str;
 
-pub mod print_position;
-
-/// A print position is a slice of string which a user would read as a single 'character'
-/// occupying one visible position on a printed page or screen.  
-/// The slice contains an extended grapheme cluster as defined by 
-/// [UAX#29](http://www.unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries)
-/// surrounded by any 
-/// [ANSI escape codes](https://en.wikipedia.org/wiki/ANSI_escape_code#Description)
-/// found in the string that do not consume visible space.  
-/// 
-/// This is a handy concept for laying out text in an Ansi-compatible console screen, 
-/// where ANSI SGR codes are used to set text colors and intensity.  
-/// The length on screen of a print position is 1, by definition.
-/// You can use arithmetic based on these lengths to pad and align text on the page or screen.
-///  
-/// With the presumption that most codes change the rendering of the characters that *follow*,
-/// most codes are embeded *at the beginning* of the [PrintPosition].  
-/// However, codes that *reset* special rendering modes, such as SGR reset and RIS,
-/// are embedded *at the end* of the [PrintPosition].  
-/// This allows caller to insert an undecorated string into the middle of a 
-/// decorated string without having to track the rendering state, so long as the 
-/// insertion is done on print position boundaries.
-/// 
-/// However, ANSI control codes and escape sequences can move the cursor on a compatible device.
-/// These characters will be passed through without modification to the output slice,
-/// but their rendering effect will not be accounted for in the print position arithmetic.
-/// This handling for backspace and tab is arguably a bug.  
-/// 
-/// Implementation note:
-/// Should there be an iterator which explicitly yields [PrintPosition]? 
-/// It is defined here primarily as a pedagogic device, to explain the iterator logic.
-/// But the iterator itself returns slice of string for efficiency.
-/// 
-/// 
-
-/*
-pub struct PrintPositionIndices<'a>(&'a str);
-
-/// Iterate over input string, yielding [&str] which segment it on [PrintPosition] boundaries.
-pub trait PrintPositionSegmentation {
-    /// Returns an iterator over the print positions found in a string.
-    fn PrintPositionIndices<'a>(&'a self) -> PrintPositionIndices<'a> ;
-}
-
-impl PrintPositionSegmentation for str {
-    fn PrintPositionIndices<'a>(&'a self) -> PrintPositionIndices<'a> {
-         new_printPositionIndices(self)
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some((_, s)) = self.0.next() {
+            Some(s)
+        } else {
+            None
+        }
     }
-
-    
 }
-*/
+
+#[inline]
+pub fn print_positions<'a>(s: &'a str) -> PrintPositions<'a> {
+    PrintPositions(print_position_indices(s))
+}
+/// Iterator returns the print position string and its offset in the containing string.
+///
+#[derive(Clone)]
+pub struct PrintPositionIndices<'a> {
+    // the victim string -- all outputs are slices of this.
+    string: &'a str,
+    // offset of beginning of slice currently being assembled or last returned.
+    cur_offset: usize,
+    // offset of the first unexamined char
+    next_offset: usize,
+    // wrapped grapheme (== extended grapheme cluster) iterator
+    gi_iterator: GraphemeIndices<'a>,
+}
+/// Factory method to create a new [PrintPositionIndices] iterator
+///
+#[inline]
+pub fn print_position_indices<'a>(s: &'a str) -> PrintPositionIndices<'a> {
+    let iter = UnicodeSegmentation::grapheme_indices(s, true);
+    PrintPositionIndices {
+        string: s,
+        cur_offset: 0,
+        next_offset: 0,
+        gi_iterator: iter,
+    }
+}
+
+impl<'a> PrintPositionIndices<'a> {
+    /// View the underlying data (the part yet to be iterated) as a slice of the original string.
+    ///
+    /// ```rust
+    /// # use print_positions::print_position_indices;
+    /// let mut iter = print_position_indices("abc");
+    /// assert_eq!(iter.as_str(), "abc");
+    /// iter.next();
+    /// assert_eq!(iter.as_str(), "bc");
+    /// iter.next();
+    /// iter.next();
+    /// assert_eq!(iter.as_str(), "");
+    /// ```
+    #[inline]
+    pub fn as_str(&self) -> &'a str {
+        &self.string[self.cur_offset..self.string.len()]
+    }
+}
+
+impl<'a> Iterator for PrintPositionIndices<'a> {
+    type Item = (usize, &'a str);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next_offset > self.string.len() {
+            return None;
+        };
+
+        enum EscapeState {
+            Normal,
+            EscapeSeen, // just saw an escape, start accumulating
+            CSISeen,    // 2nd char not terminal, continue accumulating
+        }
+
+        let mut escape_state = EscapeState::Normal;
+
+        while self.next_offset < self.string.len() {
+            let grap = self.gi_iterator.next().expect("already checked not at EOS");
+            debug_assert_eq!(
+                grap.0, self.next_offset,
+                "offset of retrieved grap not at start of rest of string"
+            );
+            self.next_offset += grap.1.len();
+
+            let ascii_byte = grap.1.as_bytes()[0];
+
+            match escape_state {
+                EscapeState::Normal => {
+                    if ascii_byte == 0x1b {
+                        escape_state = EscapeState::EscapeSeen;
+                    } else {
+                        break; // terminate the grapheme
+                    }
+                }
+
+                EscapeState::EscapeSeen => match ascii_byte {
+                    b'[' => {
+                        escape_state = EscapeState::CSISeen;
+                    }
+                    0x40..=0x5F => {
+                        // terminate escape, but continue accumulating
+                        escape_state = EscapeState::Normal;
+                    }
+                    _ => {
+                        debug_assert!(
+                            false,
+                            "unexpected char {ascii_byte} following ESC, terminating escape"
+                        );
+                        escape_state = EscapeState::Normal;
+                    }
+                },
+
+                EscapeState::CSISeen => {
+                    if (0x40..=0x7f).contains(&ascii_byte) {
+                        // end of CSI, but continue accumulating
+                        escape_state = EscapeState::Normal;
+                    } else if (0x20..=0x3f).contains(&ascii_byte) { // accumulate CSI
+                    } else {
+                        debug_assert!(
+                            false,
+                            "unexpected char {ascii_byte} in CSI sequence, terminating escape"
+                        );
+                        escape_state = EscapeState::Normal;
+                    }
+                }
+            }
+        }
+
+        // before returning, peek ahead and see whether there's a reset escape sequence we can append.
+        // there are 3 ANSI reset sequences.
+        // if, perversely, there is more than one in sequence, we'll just take one and
+        // leave the others for the beginning of the next iteration.
+
+        while self.next_offset < self.string.len()
+            && self.string.as_bytes()[self.next_offset] == 0x1b
+        {
+            if self.next_offset + 2 <= self.string.len()
+                && self.string[self.next_offset..].starts_with("\x1bc")
+            {
+                self.next_offset += 2;
+                self.gi_iterator.next();
+                self.gi_iterator.next();
+            } else if self.next_offset + 3 <= self.string.len()
+                && self.string[self.next_offset..].starts_with("\x1b[m")
+            {
+                self.next_offset += 3;
+                self.gi_iterator.next();
+                self.gi_iterator.next();
+                self.gi_iterator.next();
+            } else if self.next_offset + 4 <= self.string.len()
+                && self.string[self.next_offset..].starts_with("\x1b[0m")
+            {
+                self.next_offset += 4;
+                self.gi_iterator.next();
+                self.gi_iterator.next();
+                self.gi_iterator.next();
+                self.gi_iterator.next();
+            } else {
+                break; // not one we're interested in at end of grap.
+            }
+        }
+        // return everything between start and end offsets
+        let retlen = self.next_offset - self.cur_offset;
+        if retlen <= 0 {
+            return None;
+        } else {
+            let retval = (
+                self.cur_offset,
+                &self.string[self.cur_offset..self.next_offset],
+            );
+            // advance start to one beyond end of what we're returning
+            self.cur_offset = self.next_offset;
+            return Some(retval);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[allow(unused_imports)]
+    use anyhow::{anyhow, Result};
 
-    
+    fn esc_sgr_reset0() -> &'static str {
+        "\x1b[0m"
+    }
+    fn esc_sgr_reset() -> &'static str {
+        "\x1b[m"
+    }
+    fn esc_sgr_color() -> &'static str {
+        "\x1b[1;3m"
+    }
+
+    // test both flavors of iterator for one scenario
+    fn run_test(input: &[&str], expected: &[(usize, &str)]) -> Result<()> {
+        #[allow(unused_mut)]
+        let mut test_input = input.join("");
+        let mut observed: Vec<(usize, &str)> = vec![];
+
+        for (offset, substring) in print_position_indices(&test_input) {
+            if observed.len() > 0 {
+                let last_off = observed.last().expect("length checked").0;
+                assert!(
+                    offset > last_off,
+                    "new offset({offset}) not greater than last seen ({last_off})"
+                );
+            };
+            assert!(substring.len() > 0, "empty substring returned");
+            observed.push((offset, substring));
+        }
+
+        assert_eq!(expected, observed);
+
+        let mut observed: Vec<&str> = vec![];
+
+        for substring in print_positions(&test_input) {
+            assert!(
+                substring.len() > 0,
+                "empty substring returned (print_positions)"
+            );
+            observed.push(substring);
+        }
+
+        assert_eq!(
+            expected.len(),
+            observed.len(),
+            "comparing print positions iterator length"
+        );
+        for (exp, obs) in expected.iter().zip(observed) {
+            assert_eq!(exp.1, obs, "comparing print positions individual returns");
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn empty_string() -> Result<()> {
+        run_test(&vec![], &vec![])
+    }
+    #[test]
+    fn simple1() -> Result<()> {
+        //let test_string = ["abc", esc_sgr_color(), "def", esc_sgr_reset0()].join("");
+        let test_input = ["abc", esc_sgr_color(), "def"];
+        let e1 = [esc_sgr_color(), "d"].join("");
+        let expect = vec![(0, "a"), (1, "b"), (2, "c"), (3, &e1), (10, "e"), (11, "f")];
+
+        run_test(&test_input, &expect)
+    }
+    #[test]
+    fn trailing_reset() -> Result<()> {
+        //let test_input = ["abc", esc_sgr_color(), "def", esc_sgr_reset0()];
+        let test_input = ["ef", esc_sgr_reset0()];
+        let e2 = ["f", esc_sgr_reset0()].join("");
+        //let expect = vec![(0, "a"), (1, "b"), (2, "c"), (3, &e1), (10, "e"), (11, "f"), (12, &e2)];
+        let expect = vec![(0, "e"), (1, &e2)];
+
+        run_test(&test_input, &expect)
+    }
+    #[test]
+    fn embedded_csi_and_trailing_reset() -> Result<()> {
+        let test_input = ["abc", esc_sgr_color(), "def", esc_sgr_reset()];
+        //let test_input = [ "f", esc_sgr_reset0()];
+        let e1 = [esc_sgr_color(), "d"].join("");
+        let e2 = ["f", esc_sgr_reset()].join("");
+        let expect = vec![(0, "a"), (1, "b"), (2, "c"), (3, &e1), (10, "e"), (11, &e2)];
+        //let expect = vec![(0, &e2)];
+
+        run_test(&test_input, &expect)
+    }
+
+    #[test]
+    fn double_trailing_reset() -> Result<()> {
+        let test_input = [
+            "abc",
+            esc_sgr_color(),
+            "def",
+            esc_sgr_reset(),
+            esc_sgr_reset0(),
+            "g",
+        ];
+        let e1 = [esc_sgr_color(), "d"].join("");
+        let e2 = ["f", esc_sgr_reset(), esc_sgr_reset0()].join("");
+        let expect = vec![
+            (0, "a"),
+            (1, "b"),
+            (2, "c"),
+            (3, &e1),
+            (10, "e"),
+            (11, &e2),
+            (19, "g"),
+        ];
+
+        run_test(&test_input, &expect)
+    }
+
+    // fuzz testing found a problem with this input: [45, 27, 91, 109, 221, 133]
+    // but it doesn't fail in test even with nightly compiler --release vs --test?
+    #[test]
+    fn error_from_fuzz_test() -> Result<()> {
+        //let test_input = ["-\x1b[0m\u{dd}\u{85}"];
+        let input = ["\u{d1}\u{97}\x1b[m\u{d2}\u{83}"];
+        let expected = vec![
+            (0, "\u{d1}"),
+            (2, "\u{97}\x1b[m"),
+            (7, "\u{d2}"),
+            (9, "\u{83}"),
+        ];
+
+        run_test(&input, &expected)
+    }
+
+    #[test]
+    fn new_line_tests() -> Result<()> {
+        // unicode standard says \r\n is a single grapheme.  But separately? or \n\r?
+        let input = ["\r\n", "\na\rb", "\n\r", "\r\n"];
+        let expected = vec![
+            (0, "\r\n"),
+            (2, "\n"),
+            (3, "a"),
+            (4, "\r"),
+            (5, "b"),
+            (6, "\n"),
+            (7, "\r"),
+            (8, "\r\n"),
+        ];
+
+        run_test(&input, &expected)
+    }
+
+    // testing the whole zoo of Unicode is somebody else's problem
+    // but we do test at least test some multi-byte unicode and some grapheme clusters
+
+    #[test]
+    fn unicode_multibyte_mixed_tests() -> Result<()> {
+        // samples from UnicodeSegmentation "a̐éö̲"; // 3 bytes each
+
+        let input = ["a", esc_sgr_color(), "a̐é", esc_sgr_reset(), esc_sgr_reset()];
+        let e1 = [esc_sgr_color(), "a̐"].join("");
+        let e2 = ["é", esc_sgr_reset(), esc_sgr_reset()].join("");
+        let expected = vec![(0, "a"), (1, &e1), (10, &e2)];
+
+        run_test(&input, &expected)
+    }
 }
